40338b51dbc961d7dc3ba7a8db4d763daf3044cd
Mohammad Raihanul Hoque Sabuj <raihan.sabuj@yahoo.com>
2022-06-18 18:04:21 +0600

    updated

--- script.js
+++ script.js
@@ -12,6 +12,41 @@ const p2BtnElm = document.querySelector('#p2Btn')
 const resetBtn = document.querySelector('#resetBtn')
 const formElm = document.querySelector('form')
 
+//Winnning Score
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let gameOver = false;
+const players = ['p1', 'p2'];//
+let turnPlayer =players[Math.floor(Math.random() * players.length)];
+
+ //Update wining score
+playingToElm.textContent = winningScore;
+
+console.log(turnPlayer)
+//disable playerBtn based on Turn
+turnPlayer === 'p1'
+? p2BtnElm.setAttribute('disabled', 'disabled')
+: p1BtnElm.setAttribute('disabled', 'disabled')
+
+
+function actionReset(){
+    //set all scores to default 
+    p1Score = 0;
+    p2Score = 0;
+    winningScore = 5;
+    gameOver = false;
+
+    //Set all element's textContent and style
+    playingToElm.textContent = winningScore
+    p1ScoreElm.textContent = p1Score
+    p2ScoreElm.textContent = p2Score
+    p2ScoreElm.style.color = 'black' 
+    p1ScoreElm.style.color = 'black'
+    //withdraw disabled attribute from both of the buttons
+    p1BtnElm.removeAttribute('disabled')
+    p2BtnElm.removeAttribute('disabled')
+}
 function inputValidation(score){
     if(score < 1)
         return alert("Please provide a positive value")
@@ -21,46 +56,70 @@ function inputValidation(score){
 //inputting and updating playing to score
 formElm.addEventListener("submit", (evt)=>{
     evt.preventDefault();
+    //Reset all
+    actionReset()
     //getting the input
-    const inputScore = inputScoreElm.value
+    const inputScore = Number(inputScoreElm.value)
     //validation check
     inputValidation(inputScore)
+    //Saving it into Data Layer
+    winningScore = inputScore
+
     playingToElm.textContent = inputScore
     inputScoreElm.value= ''
-    //
-    
-
-    // console.log(inputScoreElm.value);
+   
 })
-p1BtnElm.addEventListener('click', ()=>{
-    const scoreLimit = Number(playingToElm.textContent);
-    const p1Score = Number(p1ScoreElm.textContent);
-    const p2Score = Number(p2ScoreElm.textContent);
-    if(scoreLimit > p1Score && scoreLimit > p2Score){
-        p1ScoreElm.textContent = p1Score + 1
-    }else if(scoreLimit === p1Score){
+
+function handleWiningState(){
+    if(p1Score === winningScore){
+        gameOver === true
+        //Message and color change
+        alert("Player 1 win");
         p1ScoreElm.style.color = 'green' 
         p2ScoreElm.style.color = 'red' 
-        alert("Player 1 win");
-    }
-})
-
-p2BtnElm.addEventListener('click', ()=>{
-    const scoreLimit = Number(playingToElm.textContent);
-    const p1Score = Number(p1ScoreElm.textContent);
-    const p2Score = Number(p2ScoreElm.textContent);
-    if(scoreLimit > p1Score && scoreLimit > p2Score ){
-        p2ScoreElm.textContent = p2Score + 1
-    }else if(scoreLimit === p2Score){
+    }else if(p2Score === winningScore){
+        gameOver === true
+        //Message and color change
+        alert("Player 2 win");
         p2ScoreElm.style.color = 'green' 
         p1ScoreElm.style.color = 'red' 
-        alert("Player 2 win");
+    }  
+    if(p1Score === winningScore || p2Score === winningScore){
+        p1BtnElm.setAttribute('disabled', 'disabled')
+        p2BtnElm.setAttribute('disabled', 'disabled')
     }
+}
+
+
+p1BtnElm.addEventListener('click', (evt)=>{
+    // increase the count and update dom
+        if(turnPlayer=== 'p1' && !gameOver && p1Score < winningScore){
+            p1Score++;
+            //Update Dom
+            p1ScoreElm.textContent = p1Score;
+            //Change Player Turn
+            turnPlayer = 'p2'
+            //disable Player 1
+            p1BtnElm.setAttribute('disabled', 'disabled')
+            //Enable Player 2
+            p2BtnElm.removeAttribute('disabled')
+        }
+        handleWiningState()
 })
 
+p2BtnElm.addEventListener('click', (evt)=>{
+    // increase the count and update dom
+    if(turnPlayer=== 'p2' && !gameOver && p2Score < winningScore){
+        p2Score++;
+        p2ScoreElm.textContent = p2Score;
+        turnPlayer = 'p1'
+        p2BtnElm.setAttribute('disabled', 'disabled')
+        p1BtnElm.removeAttribute('disabled')
+    }
+    handleWiningState()    
+})
+
+//Listening to Reset Button
 resetBtn.addEventListener('click', ()=>{
-    inputScoreElm.value = ''
-    playingToElm.textContent = 0
-    p1ScoreElm.textContent = 0
-    p2ScoreElm.textContent = 0
+    actionReset()
 })
